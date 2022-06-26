7f1e964e66e16a225e292874682610391effbe34
vantri <vantriktpm@gmail.com>
2022-06-26 11:18:18 +0700

    Commit Models

---
+++ Demo/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Author { get; set; }
+        public DateTime Created { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
